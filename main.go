f3df3f48881b862c3dbd7ed7338b91f1621f053e
arunima1319 <arunimakumar7@gmail.com>
2026-06-01 19:52:02 +0530

    Add additional handlers

--- main.go
+++ main.go
@@ -2,16 +2,48 @@ package main
 
 import (
 	"net/http"
+	"log"
+	"sync/atomic"
+	"fmt"
 )
 
-func main() { 
+type apiConfig struct{ 
+	fileServerHits atomic.Int32
+}
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
 
-	serveMux := http.NewServeMux()
+	apiCfg := apiConfig{}
 
-	server := http.Server{
-		Addr: ":8080",
-		Handler: serveMux,
+	mux := http.NewServeMux()
+	mux.Handle("/app/", 
+			apiCfg.middleWareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("/healthz", readinessEndpoint)
+	mux.HandleFunc("/metrics", apiCfg.displaysNumberOfRequests)
+	mux.HandleFunc("/reset", apiCfg.resetHits)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
 	}
 
-	server.ListenAndServe()
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func readinessEndpoint (w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+	
+}
+
+func(cfg *apiConfig) displaysNumberOfRequests (w http.ResponseWriter, r *http.Request){ 
+	fmt.Fprintf(w, "Hits: %d", cfg.fileServerHits.Load())
+}
+
+func (cfg *apiConfig) resetHits (w http.ResponseWriter, r *http.Request){
+	cfg.fileServerHits.Store(0)
+	w.Write([]byte("has been reset"))
 }
---
+++ middleware.go
@@ -0,0 +1,11 @@
+package main 
+
+import (
+	"net/http"
+)
+func (cfg *apiConfig) middleWareMetricsInc(next http.Handler) http.Handler{ 
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
